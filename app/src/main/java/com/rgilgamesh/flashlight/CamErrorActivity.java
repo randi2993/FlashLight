077f522b8fc0a34abf345c6c38e6ebe6b8117123
Randi Ortiz <randi2993@gmail.com>
2020-06-08 12:33:28 -0400

    [Upload] adding the project to the repo. pre-release version. Min Api Level: 16

---
+++ app/src/main/java/com/rgilgamesh/flashlight/CamErrorActivity.java
@@ -0,0 +1,36 @@
+package com.rgilgamesh.flashlight;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+public class CamErrorActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camerror);
+
+        Button btn = (Button) findViewById(R.id.btnBack);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private boolean validatePermission() {
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+        return false;
+    }
+}
